201721b5de09c28ac6a6235f0b51dec575f0257a
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-24 15:59:04 -0500

    added line about my heart

--- cookieWiggle.rb
+++ cookieWiggle.rb
@@ -1,3 +1,4 @@
 puts("My dreams are full of sweet images")
 puts("They crisp my vision with the touch of baked goodness.")
 puts("Now I can see with confectionary perfection!")
+puts("My heart pumps caramel through a system of candy canes and cupcakes.")
