d9d8b4c49e963b6de87d3b1038c5ebe1a462ce91
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-24 16:57:54 -0500

    Added lines about beer, the fuel of the gods

--- cookieWiggle.rb
+++ cookieWiggle.rb
@@ -2,3 +2,7 @@ puts("My dreams are full of sweet images")
 puts("They crisp my vision with the touch of baked goodness.")
 puts("Now I can see with confectionary perfection!")
 puts("My heart pumps caramel through a system of candy canes and cupcakes.")
+
+puts("Alas, man cannot thrive on confections alone")
+puts("It is beer that I crave.")
+puts("It is the fuel of GODS")
