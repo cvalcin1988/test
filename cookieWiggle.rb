f652f8c06f804afed496afbdbe9772af50e96782
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-24 15:32:03 -0500

    changed the name of tittleWiggle ->cookieWiggle

---
+++ cookieWiggle.rb
@@ -0,0 +1,2 @@
+print("My dreams are full of sweet images")
+print("They crisp my vision with the touch of baked goodness.")
--- tittyWiggle.rb
+++
@@ -1,2 +0,0 @@
-print("My dreams are full of heavenly images")
-print("Oh how they wiggle.")
