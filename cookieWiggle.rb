e8e5730cd14b98c892df7e77f08d44c2fcb789fd
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-25 12:35:23 -0500

    trying to force a merge conflict

--- cookieWiggle.rb
+++ cookieWiggle.rb
@@ -1,21 +1,3 @@
-puts("My dreams are full of sweet images")
-puts("They crisp my vision with the touch of baked goodness.")
-puts("Now I can see with confectionary perfection!")
-puts("My heart pumps caramel through a system of candy canes and cupcakes.")
-puts("My body is made of ICE CREAM CAKE, I excrete yogurt when I defecate")
-
-puts("Alas, man cannot thrive on confections alone")
-puts("It is beer that I crave.")
-puts("It is the fuel of GODS, and I will be...")
-puts("YES I'LL BE...")
-puts("I see you with beer in hand, and cupcake in other.")
-puts("You might have POTENTIAL.")
-
-# TIGOLBITTIES
-# TIGOLBITTIES
-# TIGOLBITTIES
-# TIGOLBITTIES
-
 let name;
 puts ("Give me Your name Mortal")
 gets.to(name)
