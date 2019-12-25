82da3a9eef7f3f95688cb4a6f40b96d05c889734
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-25 11:53:17 -0500

    pump till completion

--- cookieWiggle.rb
+++ cookieWiggle.rb
@@ -17,4 +17,5 @@ puts(name + ", you stand before the Spirit of the FEAST!")
 puts("Take up your utensil! Make your enemies feel the might of your fork!"
 puts("SPOON THEM TO DEATH!")
 
-puts("Spread your custard from land to land!")
+puts("Yes spirit. I will spread my custard from land to land!")
+puts("I will pump them till completion!")
