a4bfc9f4e641554bf3fb7ec32590edd0dc596b77
cvalcin1988 <cvalcin1988@gmail.com>
2019-12-25 12:33:57 -0500

    trying to force a merge conflict

--- cookieWiggle.rb
+++ cookieWiggle.rb
@@ -15,15 +15,3 @@ puts("You might have POTENTIAL.")
 # TIGOLBITTIES
 # TIGOLBITTIES
 # TIGOLBITTIES
-
-let name;
-puts ("Give me Your name Mortal")
-gets.to(name)
-puts(name + ", you stand before the Spirit of the FEAST!")
-puts("Take up your utensil! Make your enemies feel the might of your fork!"
-puts("SPOON THEM TO DEATH!")
-
-puts("Yes spirit. I will spread my custard from land to land!")
-puts("I will pump them till completion!")
-  
-puts("You've done well, mortal.")
